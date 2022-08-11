9c868a5d6303a26285ef8ba1383de04ca854bf3d
lihaohao <2495041749@qq.com>
2022-08-11 13:35:43 +0800

    feat: 交换实现

--- 链表/24. 两两交换链表中的节点/Option 1.js
+++ 链表/24. 两两交换链表中的节点/Option 1.js
@@ -12,6 +12,8 @@
 var swapPairs = function (head) {
   if (head === null || head.next === null) return head
 
+  // 1. 将两个元素理解为一个整体
+  // 2. 两个元素各自交换各自的
   const headAfter = head.next
   const newHead = swapPairs(headAfter.next)
 
