396a540dfb1101a21ded29398496294af5bb74ef
lihaohao <2495041749@qq.com>
2022-08-24 09:48:08 +0800

    feat: 104. 二叉树的最大深度

---
+++ 二叉树/104. 二叉树的最大深度/Option 1.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (root === null) return 0
+
+  const leftNum = maxDepth(root.left)
+  const rightNum = maxDepth(root.right)
+
+  return Math.max(leftNum, rightNum) + 1
+}
