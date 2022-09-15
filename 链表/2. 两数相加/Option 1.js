c86d490cd1fbd9f74b6a36cd90d899b4ebb716f8
lihaohao <2495041749@qq.com>
2022-09-15 23:01:51 +0800

    feat: 2022/09/15 算法打卡

--- 链表/2. 两数相加/Option 1.js
+++ 链表/2. 两数相加/Option 1.js
@@ -11,29 +11,29 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-  let prev = 0
-  const dummyHead = new ListNode(-1)
-  p = dummyHead
+  let prev = 0;
+  const dummyHead = new ListNode(-1);
+  p = dummyHead;
 
   const addMerge = (l1, l2) => {
-    if (l1 === null && l2 === null) return
+    if (l1 === null && l2 === null) return;
 
-    const val1 = l1 ? l1.val : 0
-    const val2 = l2 ? l2.val : 0
+    const val1 = l1 ? l1.val : 0;
+    const val2 = l2 ? l2.val : 0;
 
-    let add = val1 + val2 + prev
-    prev = (add / 10) | 0
-    add = add % 10
+    let add = val1 + val2 + prev;
+    prev = (add / 10) | 0;
+    add = add % 10;
 
-    const node = new ListNode(add)
-    p.next = node
-    p = p.next
+    const node = new ListNode(add);
+    p.next = node;
+    p = p.next;
 
-    addMerge(l1 ? l1.next : null, l2 ? l2.next : null)
-  }
-  addMerge(l1, l2)
+    addMerge(l1 ? l1.next : null, l2 ? l2.next : null);
+  };
+  addMerge(l1, l2);
 
-  if (prev > 0) p.next = new ListNode(prev)
+  if (prev > 0) p.next = new ListNode(prev);
 
-  return dummyHead.next
-}
+  return dummyHead.next;
+};
