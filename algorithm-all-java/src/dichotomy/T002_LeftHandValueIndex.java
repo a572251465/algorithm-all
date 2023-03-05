eeb4b371cbeedb2acde95d77a1215c727b05ca77
lihaohao <2495041749@qq.com>
2023-03-05 15:11:52 +0800

    feat: 二分法 给定一个升序数组以及一个值value，找到<=value 最右边的位置

--- algorithm-all-java/src/dichotomy/T002_LeftHandValueIndex.java
+++ algorithm-all-java/src/dichotomy/T002_LeftHandValueIndex.java
@@ -1,18 +1,18 @@
 package dichotomy;
 
 /**
- * 给定一个升序数组以及一个value值，找到>=value的做左侧值
+ * 给定一个升序数组以及一个value值，找到>=value最左侧位置
  * @author  lihh
  */
-public class T002_LeftMinValueIndex {
+public class T002_LeftHandValueIndex {
     public static void main(String[] args) {
-        System.out.println(leftMinValueIndex(2, new int[]{1, 2, 2, 4, 6, 7, 10}));
-        System.out.println(leftMinValueIndex(3, new int[]{1, 2, 2, 4, 6, 7, 10}));
-        System.out.println(leftMinValueIndex(11, new int[]{1, 2, 2, 4, 6, 7, 10}));
-        System.out.println(leftMinValueIndex(1, new int[]{2, 2, 4, 6, 7, 10}));
+        System.out.println(leftHandValueIndex(1, new int[]{2, 2, 4, 6, 7, 10}));
+        System.out.println(leftHandValueIndex(3, new int[]{1, 2, 2, 4, 6, 7, 10}));
+        System.out.println(leftHandValueIndex(11, new int[]{1, 2, 2, 4, 6, 7, 10}));
+        System.out.println(leftHandValueIndex(1, new int[]{2, 2, 4, 6, 7, 10}));
     }
 
-    public static int leftMinValueIndex(int value, int[] values) {
+    public static int leftHandValueIndex(int value, int[] values) {
         if (values == null || values.length == 0) return -1;
         int l = 0, r = values.length - 1;
         int ans = -1;
---
+++ algorithm-all-java/src/dichotomy/T003_RightHandValueIndex.java
@@ -0,0 +1,33 @@
+package dichotomy;
+
+/**
+ * 二分法 给定一个升序数组以及一个值value，找到<=value 最右边的位置
+ * @author  lihh
+ */
+public class T003_RightHandValueIndex {
+    public static void main(String[] args) {
+
+        System.out.println(rightHandValueIndex(5, new int[]{2, 4, 5, 5, 8}));
+        System.out.println(rightHandValueIndex(2, new int[]{2, 4, 5, 5, 8}));
+        System.out.println(rightHandValueIndex(10, new int[]{2, 4, 5, 5, 8, 11}));
+    }
+
+    public static int rightHandValueIndex(int value, int[] values) {
+        if (values == null || values.length == 0) return -1;
+
+        int l = 0, r = values.length - 1, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int midValue = values[mid];
+
+            if (midValue <= value) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
