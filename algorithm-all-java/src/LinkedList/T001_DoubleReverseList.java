105c09b5651fe6ffa6e2832c8853f7c62302f7c0
lihaohao <2495041749@qq.com>
2023-03-06 20:24:17 +0800

    feat: 测试双向链表反转

--- algorithm-all-java/src/LinkedList/T001_DoubleReverseList.java
+++ algorithm-all-java/src/LinkedList/T001_DoubleReverseList.java
@@ -1,7 +1,30 @@
 package LinkedList;
 
 public class T001_DoubleReverseList {
-    public DoubleListNode reverseList(DoubleListNode head) {
+    public static void main(String[] args) {
+        DoubleListNode n1 = new DoubleListNode(1);
+        DoubleListNode n2 = new DoubleListNode(2);
+        DoubleListNode n3 = new DoubleListNode(3);
+
+        n1.next = n2;
+        n2.next = n3;
+
+        n3.prev = n2;
+        n2.prev = n1;
+
+        DoubleListNode newHead = reverseList(n1);
+        while (newHead != null) {
+            if (newHead.prev != null)
+                System.out.print("前" + newHead.prev.val + "|");
+            System.out.print("中" + newHead.val + "|");
+            if (newHead.next != null)
+                System.out.println("后" + newHead.next.val + "|");
+
+            System.out.println("");
+            newHead = newHead.next;
+        }
+    }
+    public static DoubleListNode reverseList(DoubleListNode head) {
         if (head == null || head.next == null) return head;
 
         DoubleListNode next, newHead = null;
