d3aa94174b34d1c6c374064dc5ade6a93a617e8f
lihaohao <2495041749@qq.com>
2023-03-28 16:56:56 +0800

    feat: 888. 公平的糖果交换

---
+++ algorithm-all-java/src/dichotomy/T008_LeetCode888_FairCandySwap/Option01.java
@@ -0,0 +1,29 @@
+package dichotomy.T008_LeetCode888_FairCandySwap;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+// https://leetcode.cn/problems/fair-candy-swap/
+
+public class Option01 {
+  public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+    int sumA = Arrays.stream(aliceSizes).sum();
+    int sumB = Arrays.stream(bobSizes).sum();
+    int delta = (sumA - sumB) / 2;
+    Set<Integer> rec = new HashSet<Integer>();
+    for (int num : aliceSizes) {
+      rec.add(num);
+    }
+    int[] ans = new int[2];
+    for (int y : bobSizes) {
+      int x = y + delta;
+      if (rec.contains(x)) {
+        ans[0] = x;
+        ans[1] = y;
+        break;
+      }
+    }
+    return ans;
+  }
+}
